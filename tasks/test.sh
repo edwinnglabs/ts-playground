f749b930d58f92f7e60a8fdd377b944ee3c7f109
Edwin Ng <edwinnglabs@gmail.com>
2021-10-30 21:06:08 -0700

    remove the download modules for internal dev

--- tasks/test.sh
+++
@@ -1,1 +0,0 @@
-date >>  ./source/test/hello-world.log
--- ts_playground/pypi_downloads/plot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-if __name__ != 'main':
-    fig, ax = plt.subplots(1, 1, figsize=(16, 8))
-    df = pd.read_csv('../../source/clean/orbit-ml-download-clean.csv', parse_dates=['week'])
-    df['cum_downloads'] = df['num_downloads'].cumsum()
-    ax.plot(df['week'].values, df['cum_downloads'].values)
-    fig.show()
